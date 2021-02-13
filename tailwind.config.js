fd8abceef49d64f39b414051e4cd991a2d27e744
IvanMOA <ax3l.imo@gmail.com>
2021-02-12 21:32:56 -0600

    Added App colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,12 @@
 module.exports = {
-}
+  theme: {
+    extend: {
+      colors: {
+        uanlBlue: "#004A8F",
+        uanlYellow: "#FBC43D",
+        grayBlack: "#222222",
+        greyBlack30p : '#2222224D'
+      },
+    },
+  },
+};
