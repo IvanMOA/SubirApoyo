620a733e2f97da706332c55872c81aad8bb94317
IvanMOA <ax3l.imo@gmail.com>
2021-02-12 21:33:49 -0600

    Implemented some components for testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,56 @@
-import React from 'react';
-import { RecoilRoot, atom, useRecoilState, selector, useRecoilValue } from 'recoil'
+import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  useRecoilState,
+  selector,
+  useRecoilValue,
+} from "recoil";
+import { PhotoDropzone } from "./components/Dropzone/Dropzone";
+import { Modal, ModalState } from "./components/Modal/Modal";
+import { ErrorModalContent } from './components/Modal/ErrorModalContent'
 
-const counter = atom({ key: 'counter', default: 0})
+const counter = atom({ key: "counter", default: 0 });
 
 const counterPlus10 = selector({
-  key: 'counterPlus10',
-  get: ({get}) => {
-    const counterV = get(counter)
-    return counterV + 10
-  }
-})
+  key: "counterPlus10",
+  get: ({ get }) => {
+    const counterV = get(counter);
+    return counterV + 10;
+  },
+});
 
-export function Home(){
-  const [ counterV , setCounter ] = useRecoilState(counter)
-  return <div>
-    <h1>Count:  { counterV }</h1>
-    <button onClick={ () => setCounter(counterV + 1)} >++</button>
-  </div>
-}
-
-function Home2(){
-  const [ counterV , setCounter ] = useRecoilState(counter)
-  const countPlus10 = useRecoilValue(counterPlus10)
-  return <div>
-    <h1>Count: { counterV }</h1>
-    <h2>CountPlus10: {countPlus10}</h2>
-    <button onClick={ () => setCounter(counterV + 1)} >++</button>
-  </div>
+export function Home() {
+  const [counterV, setCounter] = useRecoilState(counter);
+  const [{}, setModalState] = useRecoilState(ModalState);
+  return (
+    <div>
+      <h1>Count: {counterV}</h1>
+      <button onClick={() => setCounter(counterV + 1)}>++</button>
+      <button
+        onClick={() =>
+          setModalState((currState) => ({
+            modalIsOn: true,
+            child: <ErrorModalContent title="Error" message="Hubo falla bro" />,
+          }))
+        }
+      >
+        openmodal bro (:
+      </button>
+    </div>
+  );
 }
 
 function App() {
+  const [{ modalIsOn, child }, setModalState] = useRecoilState(ModalState);
   return (
-<RecoilRoot>
-    <div className="App bg-red-800">Hello world</div>
-    <Home />
-    <Home2 />
-</RecoilRoot>
+    <div>
+      <Modal />
+      <div className="App bg-red-800">Hello world</div>
+      <Home />
+      <PhotoDropzone />
+    </div>
   );
 }
 
 export default App;
-   
