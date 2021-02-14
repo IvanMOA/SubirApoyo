a8b5ec13dfa54fd3c5271fde914a682c25e1fedb
IvanMOA <ax3l.imo@gmail.com>
2021-02-13 20:41:59 -0600

    Added firebase to the proyect

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDhuTQ-hqA9WJ7Y9uZYlxcDX4FNw6iJ-iI",
+  authDomain: "subirapoyo.firebaseapp.com",
+  projectId: "subirapoyo",
+  storageBucket: "subirapoyo.appspot.com",
+  messagingSenderId: "397185375554",
+  appId: "1:397185375554:web:28663278d64d5ee635bcdb",
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebaseApp.auth();
+export const firestore = firebaseApp.firestore();
+export const storage = firebaseApp.storage();
