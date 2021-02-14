8eec52c08aab9f0bab7afde7c1495b01a4367e7f
IvanMOA <ax3l.imo@gmail.com>
2021-02-13 21:10:58 -0600

    Readded Modal component to App, otherwise it won't work

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,22 @@ import {
 } from "recoil";
 import { PhotoDropzone } from "./components/Dropzone/Dropzone";
 import { Modal, ModalState } from "./components/Modal/Modal";
-import { ErrorModalContent } from './components/Modal/ErrorModalContent'
-import { Home } from './Pages/Home'
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { ErrorModalContent } from "./components/Modal/ErrorModalContent";
+import { Home } from "./Pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const App : React.FC = () => {
+const App: React.FC = () => {
   const [{ modalIsOn, child }, setModalState] = useRecoilState(ModalState);
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/"  render={ () => <Home /> } />
-      </Switch>
-    </Router>
+    <div>
+      <Modal />
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+        </Switch>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
