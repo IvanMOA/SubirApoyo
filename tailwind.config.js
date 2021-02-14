4f36764f4825adb0fba452c88700c9d23fb7ecfe
IvanMOA <ax3l.imo@gmail.com>
2021-02-13 20:24:06 -0600

    Added paddings for different screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         grayBlack: "#222222",
         greyBlack30p : '#2222224D'
       },
-    },
+      padding: {
+        contentSmScr : '1rem',
+        contentMdScr : '4rem',
+        contentLgScr : '6rem',
+        contentXlScr : '10rem'
+      }
+    }, 
   },
 };
