80a8e8b2751fad4bf10815f88c8c9c6366409240
IvanMOA <ax3l.imo@gmail.com>
2021-02-14 14:07:09 -0600

    Completed Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { contentPadding } from '../../theme/theme'
+import { Link } from "react-router-dom"
 
 export const Navbar : React.FC = () => {
     return <div className={ `${contentPadding} w-full h-20   bg-uanlBlue  flex justify-between items-center` }>
         <div>
             <h1 className="text-white text-lg">Título de la página</h1>
         </div>
-        <div className="text-gray-200">
-            
+        <div className="">
+            <NavbarLink content="Home" to="/" />
         </div>
     </div>
 } 
+
+type NavbarLinkProps = {
+    to: string
+    content: string
+}
+
+const NavbarLink : React.FC<NavbarLinkProps> = ({ to, content }) => {
+    return <Link children={content} to={to} className="text-gray-200 hover:text-white transition-colors duration-100" />
+}
 
