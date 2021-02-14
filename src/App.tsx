6f4c92566e2d5bac1932537c94e6d297dae43c85
IvanMOA <ax3l.imo@gmail.com>
2021-02-13 20:24:21 -0600

    Removed code that was used for testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,47 +9,17 @@ import {
 import { PhotoDropzone } from "./components/Dropzone/Dropzone";
 import { Modal, ModalState } from "./components/Modal/Modal";
 import { ErrorModalContent } from './components/Modal/ErrorModalContent'
+import { Home } from './Pages/Home'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
-const counter = atom({ key: "counter", default: 0 });
-
-const counterPlus10 = selector({
-  key: "counterPlus10",
-  get: ({ get }) => {
-    const counterV = get(counter);
-    return counterV + 10;
-  },
-});
-
-export function Home() {
-  const [counterV, setCounter] = useRecoilState(counter);
-  const [{}, setModalState] = useRecoilState(ModalState);
-  return (
-    <div>
-      <h1>Count: {counterV}</h1>
-      <button onClick={() => setCounter(counterV + 1)}>++</button>
-      <button
-        onClick={() =>
-          setModalState((currState) => ({
-            modalIsOn: true,
-            child: <ErrorModalContent title="Error" message="Hubo falla bro" />,
-          }))
-        }
-      >
-        openmodal bro (:
-      </button>
-    </div>
-  );
-}
-
-function App() {
+const App : React.FC = () => {
   const [{ modalIsOn, child }, setModalState] = useRecoilState(ModalState);
   return (
-    <div>
-      <Modal />
-      <div className="App bg-red-800">Hello world</div>
-      <Home />
-      <PhotoDropzone />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/"  render={ () => <Home /> } />
+      </Switch>
+    </Router>
   );
 }
 
