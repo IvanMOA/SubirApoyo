0b3164f3065203a0afc200adaf7d79368e86544e
IvanMOA <ax3l.imo@gmail.com>
2021-02-17 17:50:49 -0600

    Added functions to interact with Employee collection in firebsae

---
+++ src/store/EmployeeData.tsx
@@ -0,0 +1,49 @@
+import { atom } from "recoil";
+import { firestore, photosRef } from '../firebase'
+import { DocumentSnapshot } from '@firebase/firestore-types'
+const employeesCollection = firestore.collection('employees')
+
+export interface Employee {
+  employeeNumber: string;
+  dependency: string;
+  photoURL: string;
+  gafettePhotoURL: string;
+}
+
+export const EmployeeState = atom<Employee>({
+  key: "employee",
+  default: { employeeNumber: "", dependency: "", photoURL: "", gafettePhotoURL: "" },
+});
+
+export const getEmployee =  async ( employeeNumber : string ) => {
+    const doc = await employeesCollection.doc(employeeNumber).get()
+    return doc
+} 
+
+export const createNewEmployee = async (employee : Employee) => {
+        await employeesCollection.doc(employee.employeeNumber).set(employee)
+        return employee
+}
+
+export const updatePhotoURL = async (employeeNumber : string, photoURL: string) => {
+    await employeesCollection.doc(employeeNumber).update({photoURL: photoURL})
+}
+
+export const updateGafettePhotoURL = async (employeeNumber : string, gafettePhotoURL: string) => {
+    await employeesCollection.doc(employeeNumber).update({gafettePhotoURL})
+}
+
+export const loginWithEmployeeCredentials = async (employeeNumber : string, dependency: string) : Promise<Employee> => {
+    let emp
+    const doc = await  getEmployee(employeeNumber)
+    if( !doc.exists ){
+        emp = await createNewEmployee({ dependency, employeeNumber, photoURL: '', gafettePhotoURL: ''})
+    }else{
+        emp = doc.data() as Employee
+    }
+    return emp
+}
+
+export const uploadProfilePhoto = (imgData : string) => {
+    
+}
