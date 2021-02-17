4eb3a23a84c255a4d537556f5f1bbb2df4c3603b
IvanMOA <ax3l.imo@gmail.com>
2021-02-17 17:46:45 -0600

    Formatted file

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
 import React from "react";
-import {
-  RecoilRoot,
-  atom, 
-  useRecoilState,
-  selector,
-  useRecoilValue,
-} from "recoil";
-import { PhotoDropzone } from "./components/Dropzone/Dropzone";
-import { Modal, ModalState } from "./components/Modal/Modal";
-import { ErrorModalContent } from "./components/Modal/ErrorModalContent";
+import { Modal } from "./components/Modal/Modal";
 import { Home } from "./Pages/Home";
-import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
-import { Location } from 'history'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 import { Start } from "./Pages/Start";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-
-const MainAnimatedSwitch = withRouter(( { location} ) => {
-  return  <TransitionGroup><CSSTransition key={location.key}  timeout={500} classNames="slidevertical">
-        <Switch location={ location }>
+const MainAnimatedSwitch = withRouter(({ location }) => {
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        timeout={500}
+        classNames={"slidevertical"}
+      >
+        <Switch location={location}>
           <Route exact path="/" render={() => <Start />} />
-          <Route exact path="/inicio" render={() => <Home />} />
+          <Route path="/inicio" render={() => <Home />} />
         </Switch>
-      </CSSTransition></TransitionGroup>
-})
+      </CSSTransition>
+    </TransitionGroup>
+  );
+});
 
 const App: React.FC = () => {
-  const [{ modalIsOn, child }, setModalState] = useRecoilState(ModalState);
   return (
     <div>
       <Modal />
       <Router>
-    <MainAnimatedSwitch/>
+        <MainAnimatedSwitch />
       </Router>
     </div>
   );
