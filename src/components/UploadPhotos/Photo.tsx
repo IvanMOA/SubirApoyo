a7fe39bda2740cffdfc5e00ef77ea1ebcf4753b8
IvanMOA <ax3l.imo@gmail.com>
2021-02-17 17:50:23 -0600

    Removed remove button

--- src/components/UploadPhotos/Photo.tsx
+++ src/components/UploadPhotos/Photo.tsx
@@ -11,11 +11,10 @@ type PhotoProps = {
 export const Photo : React.FC<PhotoProps> = ( { src, deletePhoto }) => {
     const [{}, setModalState] = useRecoilState(ModalState)
     const showImageOnModal = () => {
-        setModalState({modalIsOn: true, child: <img style={ {maxWidth: '90vw'}} src={src} />})
-    }
+        setModalState({modalIsOn: true, child: <img style={ {maxHeight: '60vh'}} src={src} />})
+    } 
 
-    return <div className="flex relative w-32 h-32 md:w-40 md:h-40">
-        <img onClick={showImageOnModal} src={src} className="object-cover cursor-pointer"/>
-        <button onClick={() => deletePhoto()} className="absolute text-2xl text-white right-4 top-1">&times;</button>
+    return <div className="flex relative">
+        <img onClick={showImageOnModal} className="max-h-32 cursor-pointer" src={src}/>
     </div>
 }
