c584c7efe6cfe4e906bbcdd2682ae96e07d8c0e6
IvanMOA <ax3l.imo@gmail.com>
2021-02-17 17:57:19 -0600

    Moved switch from outer component to normal

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,31 +1,9 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  useRouteMatch,
-  withRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { LoginEmplDepe } from "../components/LoginEmplDepe/LoginEmplDepe";
 import { UploadDocuments } from "../components/UploadDocuments/UploadDocuments";
 import { PrivateRoute } from "../components/CustomRoutes/PrivateRoute";
-
-const HomeAnimatedSwitch = withRouter(({ location, history }) => {
-  console.log("From Second router");
-  const { url } = useRouteMatch();
-
-  return (
-    <Switch location={location}>
-      <Route
-        exact
-        path={`/inicio/credenciales`}
-        render={() => <LoginEmplDepe />}
-      />
-      <PrivateRoute path="/inicio/fotos" Component={ UploadDocuments } exact={true} />
-    </Switch>
-  );
-});
 
 export const Home: React.FC = () => {
   return (
@@ -34,7 +12,18 @@ export const Home: React.FC = () => {
         <Navbar />
         <div className="home-router-container flex flex-col align-center justify-center h-full w-full">
           <BrowserRouter>
-            <HomeAnimatedSwitch />
+            <Switch>
+              <Route
+                exact
+                path={`/inicio/credenciales`}
+                render={() => <LoginEmplDepe />}
+              />
+              <PrivateRoute
+                path="/inicio/fotos"
+                Component={UploadDocuments}
+                exact={true}
+              />
+            </Switch>
           </BrowserRouter>
         </div>
       </div>
