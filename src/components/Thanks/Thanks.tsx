469a7fdf9bbe7739381d8db880bfee93223134ef
IvanMOA <ax3l.imo@gmail.com>
2021-02-17 19:11:16 -0600

    Added thanks page

---
+++ src/components/Thanks/Thanks.tsx
@@ -0,0 +1,9 @@
+export const Thanks = () => {
+  return (
+    <div className="mx-4 flex justify-center transition-all relative">
+      <div className="bg-white w-full sm:w-1/2 md-1/3 lg:w-1/2 rounded-lg pb-4 transition-all duration-1000 overflow-hidden px-8 py-4">
+        <h1 className="text-center text-uanlYellow font-bold text-5xl md:text-7xl lg:text-9xl">Gracias por tu apoyo!</h1>
+      </div>{" "}
+    </div>
+  );
+};
