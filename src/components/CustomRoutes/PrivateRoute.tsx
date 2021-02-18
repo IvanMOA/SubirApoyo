402803db1e0dbe28b136e12b11f82917b4ddd6ba
IvanMOA <ax3l.imo@gmail.com>
2021-02-17 19:10:55 -0600

    Redirect to Thanks if alreadu supported the candidate

--- src/components/CustomRoutes/PrivateRoute.tsx
+++ src/components/CustomRoutes/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, RouteComponentProps } from "react-router";
+import { Redirect, RouteComponentProps, useHistory } from "react-router";
 import { Route } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { EmployeeState } from "../../store/EmployeeData";
@@ -11,6 +11,8 @@ type PrivateRouteProps = {
 };
 export const PrivateRoute = ({ Component, path, exact }: PrivateRouteProps) => {
   const [employee] = useRecoilState(EmployeeState);
+  const history = useHistory()
+  if(employee.supported && !history.location.pathname.includes('gracias'))  history.push('/inicio/gracias')
   return (
     <Route
       path={path}
